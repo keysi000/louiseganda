9accaa3e73df5c8cb51e4a59bc26dd0aea5a0936
keysi000 <rosinakremier90@gmail.com>
2025-03-27 17:00:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 200); 
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
